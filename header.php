5c1b9ce9f8edb90275a8babf501177850f49ab88
Amilkhael <amilkhael@outlook.com>
2018-01-09 17:49:35 -0600

    Initial commit

---
+++ header.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* 	@author Amilkhael Chávez Delgado;
+*	Documento: Header para las páginas
+*/
+?>
+<!DOCTYPE html>
+<html lang="es-419">
+<head>
+	<!--metas-->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="author" content="La Salle">
+	<meta name="description" content="Somos una red de universidades de inspiración cristiana, cuyo distintivo fundamental es la formación de Profesionales con Valor.">
+	<meta property="og:url" content="http://redlasalle.mx">
+	<meta property="og:image" content="http://redlasalle.mx/images/contenido/mapa.png">
+	<meta property="og:title" content="Red de Universidades La Salle, México">
+	<meta property="og:description" content="Somos una red de universidades de inspiración cristiana, cuyo distintivo fundamental es la formación de Profesionales con Valor.">
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+	<!-- Titulo -->
+	<title>Red de Universidades La Salle, México</title>
+
+	<!--css-->
+	<link rel="stylesheet" href="css/normalize.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/custom.min.css">
+
+	<!--[if IE]>
+			<link rel="stylesheet" href="css/ie.css">
+		<![endif]-->
+
+	<!--[if lte IE 8]>
+			<script src="js/respond/respond.js"></script>
+			<script src="js/excanvas/excanvas.js"></script>
+	<![endif]-->
+	<!--fonts-->
+	<!--js-->
+	
+	<!-- favicon -->
+	<link rel="icon" type="image/png" href="http://www.lasalle.mx/wp-content/themes/lasalle/images/favicon.ico">
+	<link rel="alternate" href="http://lasalle.mx" hreflang="es-mx" />
+
+</head>
+<body>
+	
+    <!-- HEADER-->
+    <header class="bgHeader"> 
+
+      <div class="container">
+	    	<h1>Red de Universidades <br><span class="lasalle">La Salle</span></h1>
+	    	<h2>formamos Profesionales con <span class="pvalor">Valor</span></h2>
+	      	<div class="col-sm-4"><p>Somos una red de educación, investigación e innovación con más de 50 años de trayectoria formando Profesionales con Valor.</p></div>
+	      	<div class="col-sm-4"><p>La red cuenta con 15 campus en México, nuestro sello es la calidad educativa y la verdadera formación integral.</p></div>
+	    	
+	    	
+      </div>
+      
+    </header>
+
+    <!--  FIN HEADER -->
